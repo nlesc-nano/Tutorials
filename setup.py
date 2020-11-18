0048f5d73100cf560515fddb2981dee3a155874c
felipez <tifonzafel@gmail.com>
2020-11-18 17:16:10 +0100

    reintroduced python 3.6 in the CI

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Documentation :: Sphinx',
